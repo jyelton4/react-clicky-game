fd8543a9b841d7cecb87107677ce3f84f1af3260
JacobY <jyelton2040@gmail.com>
2018-10-08 18:58:32 -0600

    Updates

--- clickygame/src/utils/API.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const BASEURL = "https://dog.ceo/api/breeds/image/random/12";
-
-export default {
-    getPhotos: function() {
-        return axios.get(BASEURL);
-    }
-};
